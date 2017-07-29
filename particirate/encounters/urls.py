bef5d7a8f1fbbc5ee83f369d28fcbddf9130a0b9
Alec van Niekerk <alecfvn@gmail.com>
2017-07-29 02:07:23 +0200

    changing file structure up a little

--- particirate/encounters/urls.py
+++ particirate/encounters/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^create/$', views.create, name='create'),
+    url(r'^index/$', views.list_games, name='list_games'),
+    url(r'^create/$', views.create_game, name='create_game'),
 ]
