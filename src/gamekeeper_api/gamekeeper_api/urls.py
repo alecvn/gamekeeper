a2b495c5292940a4edef0bc81971b8a96f77da9c
Alec <alec@implicitdesign.co.za>
2017-08-07 14:19:41 +0200

    trying to fix urls error

--- src/gamekeeper_api/gamekeeper_api/urls.py
+++ src/gamekeeper_api/gamekeeper_api/urls.py
@@ -39,8 +39,8 @@ from django.contrib import admin
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    #url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^gamekeeper/', include('gamekeeper.urls')),
-#    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include('gamekeeper.urls')),
+    #url(r'^', include(router.urls)),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
