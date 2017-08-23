f6cfad3095d3133020a3b9b8811cdb0fd63cc718
Alec van Niekerk <alecfvn@gmail.com>
2017-08-24 01:15:30 +0200

    refining api actions

--- src/gamekeeper_ui/assets/js/reducers/events.js
+++ src/gamekeeper_ui/assets/js/reducers/events.js
@@ -1,13 +1,15 @@
 import {
     FETCH_EVENTS_REQUEST,
     FETCH_EVENTS_SUCCESS,
-    FETCH_EVENTS_FAILURE
+    FETCH_EVENTS_FAILURE,
+    LOAD_EVENTS
 } from '../actions'
 
 function events(
     state = {
 	isFetching: false,
-	events: []
+	events: [],
+	focussed_events: []
     },
     action) {
     switch (action.type) {
@@ -18,7 +20,12 @@ function events(
 	case FETCH_EVENTS_SUCCESS:
 	    return Object.assign({}, state, {
 		isFetching: false,
-		events: action.events
+		events: action.events,
+		focussed_events: [action.events[0]],
+	    })
+	case LOAD_EVENTS:
+	    return Object.assign({}, state, {
+		focussed_events: state.events.filter(function(event) {return action.events.includes(event.id)}),
 	    })
 	case FETCH_EVENTS_FAILURE:
 	    return Object.assign({}, state, {
